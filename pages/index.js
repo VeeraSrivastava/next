7dc450602e7d4f41e10a5d18dce2ca11f3b3f776
Veera Srivastava <veerasriv@gmail.com>
2023-03-29 01:13:19 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export async function getStaticProps() {
     }
   }
   const snapshot = await get(ref(database, 'posts'));
-    console.log(snapshot)
+    console.log(snapshot.val())
 
 //   console.log(snapshot.val())
  
