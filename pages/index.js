a5efc56fba81096825e912d1fd57d947a22bf222
Veera Srivastava <veerasriv@gmail.com>
2023-03-28 13:15:11 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,83 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-
-export default function Home() {
+import Post from '../components/Post'
+import { sortByDate } from '../utils'
+import { getAraray } from '../getArray'
+import { database,ref,set ,onValue,get,getStorage,Sref,uploadBytes,getDownloadURL,storage, listAll,list } from "../firebase"
+export default function Home({ posts }) {
+  const tro=getAraray()
+  console.log(tro+"tro")
   return (
-    <div className="container">
+    <div>
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Dev Blog</title>
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+      <div className='posts'>
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
     </div>
   )
+}
+export async function getStaticProps() {
+  function objectToArray(obj) {
+    if (obj != null||undefined){
+    return Object.keys(obj).map(key => obj[key]);
+    }
+  }
+  const snapshot = await get(ref(database, 'posts'));
+  const data = snapshot.val();
+  console.log(data)
+  const myArray = objectToArray(data);
+  
+  // const posty = [{title: 'Post 1', date: '8 October, 3005', excerpt: 'excerpt', content: 'Content of post 1'}, {title: 'Post 2', content: 'Content of post 2'}];
+const posty=myArray;
+  posty.forEach((post) => {
+    console.log(post.title)
+    // const fileName = `${post.title}.md`;
+    const fileName = `${post.id}.md`;
+
+    console.log(fileName)
+    const filePath = path.join(process.cwd(), 'posts', fileName);
+    console.log(filePath)
+
+    // const fileContent = `# ${post.title}\n\n${post.content}`;
+    // const fileContent = `---\ntitle: ${post.title}\ndate: ${post.date} 📅\n \ncover_image: ${post.cover_image}\nexcerpt: ${post.excerpt}\n---\n\n${post.content}`;
+    const fileContent = `---\ntitle: ${post.title}\ndate: ${post.date} 📅\n \ncover_image: ${post.thumbnail}\ncoee: ${post.cover_image}\nexcerpt: ${post.excerpt}\n---\n\n${post.content}`;
+
+    fs.writeFileSync(filePath, fileContent);
+  });
+
+
+  // Get files from the posts dir
+  const files = fs.readdirSync(path.join('posts'))
+
+  // Get slug and frontmatter from posts
+  const posts = files.map((filename) => {
+    // Create slug
+    const slug = filename.replace('.md', '')
+
+    // Get frontmatter
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    )
+
+    const { data: frontmatter } = matter(markdownWithMeta)
+
+    return {
+      slug,
+      frontmatter,data,myArray
+    }
+  })
+
+  return {
+    props: {
+      posts: posts.sort(sortByDate),
+    },
+  }
 }
